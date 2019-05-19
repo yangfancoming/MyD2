a4a727cfeb054833964709ba05c99adedcba1a86
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-05-19 21:41:42 +0800

    增加 公网请求示例

--- src/views/demo/page1/index.vue
+++ src/views/demo/page1/index.vue
@@ -1,17 +1,27 @@
 <template>
   <d2-container :filename="filename">
     <template slot="header">Page 1 header</template>
-    Hello World
+      Hello World {{filename}}
+
+      <br/>
+      <el-button @click="test()">请求测试</el-button>
   </d2-container>
 </template>
 
 <script>
-export default {
-  name: 'page1',
-  data () {
-    return {
-      filename: __filename
+    export default {
+        name: 'page1',
+        data () {
+            return {
+                filename: __filename  // index.js
+            }
+        },
+        methods:{
+            test () {
+                return this.$axios.get('/mobile/get').then(res => {
+                    console.log(res)
+                }).catch(() => {})
+            },
+        }
     }
-  }
-}
 </script>
